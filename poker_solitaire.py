f507a55d556ca1e2c4e551454201d81f2c3c6ea5
caihongni <hongnicai88@gmail.com>
2026-02-20 14:23:19 +0800

    version1

---
+++ poker_solitaire.py
@@ -0,0 +1,134 @@
+from enum import Enum
+import random
+import time
+class Suite(Enum):
+    """花色（枚舉）"""
+    JOKER,SPADE,HEART,CLUB,DIAMOND=range(5)
+
+class Card:
+    """牌"""
+
+    def __init__(self, suite, face):
+        self.suite = suite
+        self.face = face
+
+    def __repr__(self):
+        suites = '🃟♠♥♣♦'
+        faces = ['', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        return f'{suites[self.suite.value]}{faces[self.face]}'
+    
+    def __lt__(self, other):
+        if self.suite == other.suite:
+            return self.face < other.face   # 花色相同比较点数的大小
+        return self.suite.value < other.suite.value   # 花色不同比较花色对应的值
+    def __eq__(self, other):
+        return self.face == other.face
+        
+    
+        
+    
+class Poker:
+    
+    """扑克"""
+
+    def __init__(self):
+        self.cards = [Card(suite, face)
+                      for suite in [Suite(i) for i in range(1,5)]
+                      for face in range(1, 14)]  # 52张牌构成的列表
+        self.current = 0  # 记录发牌位置的属性
+
+    def shuffle(self):
+        """洗牌"""
+        self.current = 0
+        random.shuffle(self.cards)  # 通过random模块的shuffle函数实现随机乱序
+
+    def deal(self):
+        """发牌"""
+        card = self.cards[self.current]
+        self.current += 1
+        return card
+
+    @property
+    def has_next(self):
+        """还有没有牌可以发"""
+        return self.current < len(self.cards)
+class Player:
+    """玩家"""
+    def __init__(self, name):
+        self.name = name
+        self.cards = []   # 玩家手上的牌
+        self.collects=[]  #玩家收回的牌
+        self.count=0    #計算收回的牌數
+
+    def get_one(self, card):
+        """摸牌"""
+        self.cards.append(card)
+
+    def draw_one(self):
+        """抽牌"""
+        return self.cards.pop()
+    
+    def collect_cards(self, desk_cards):#玩家手中收回的牌
+        """收牌"""
+        self.collects=self.collects+desk_cards
+        return len(self.collects)       #返回玩家收回的牌的總數量
+    
+    def compare_number(self, other): #與對方玩家比對收回的總牌數,返回較大者
+        if self.count >= other.count:
+            return self
+        else: 
+            return other
+def main():       
+    poker = Poker()
+    poker.shuffle()
+    players = [Player('小傑'), Player('奇亞')]
+    desk_cards=[Card(Suite.JOKER,0)]
+    # 将牌轮流发到每个玩家手上每人26张牌
+    for _ in range(26):
+        for player in players:
+            player.get_one(poker.deal())
+
+   
+    while True:        
+        for player in players:
+            answer=input("shuffle?yes or no ?please answer with y or n:")
+            
+            if answer=='y':
+                random.shuffle(player.cards)
+                print(f'{player.name}進行了一次洗牌')
+            else:
+                pass
+            time.sleep(1)
+            temp=player.draw_one()
+
+            print(f'{player.name}抽出一張牌{temp}')
+        
+            if temp.face not in list(map(lambda x: x.face, desk_cards)):
+                desk_cards.append(temp)
+            
+            else:
+                desk_cards.append(temp)
+                n=desk_cards.index(temp)
+                player.count=player.collect_cards(desk_cards[n:])
+                cards_len=len(desk_cards[n:])
+                desk_cards=desk_cards[:n]
+                print(f'碰！{player.name}收回{cards_len}張牌,手中收回的已有的牌數{player.count}張')
+            print(f'桌面上的牌：{desk_cards}')
+            
+        if not player.cards:
+            print("玩家手中都沒牌了，開始計數")
+            break
+    winner=players[0]
+    for i in range(1,len(players)):
+        winner=winner.compare_number(players[i])
+    collect_nums=[player.count for player in players]
+    indices=[index for index,value in enumerate(collect_nums) if value == winner.count]
+
+    if len(indices)!=len(players):
+        print("勝者是：")
+        for index in indices:
+            print(players[index].name,end=" ")
+    else:
+        print("平局")
+if __name__=="__main__":
+    main()
